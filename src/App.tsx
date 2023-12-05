919777ea172fee4f8b435f10dcae9ac4fc74d0bb
Kunjan <kunjan@controlshift.in>
2023-12-05 22:56:58 +0530

    routing intigrated

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,26 @@ import "./App.css";
 import ContentContainer from "./components/todoList/todoList.tsx";
 import Header from "./components/header/header.tsx";
 import CreateTodo from "./components/createTask/createTodo.tsx";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <>
       <div className="todo-app-main-container">
         <Header></Header>
-        {/* <ContentContainer></ContentContainer> */}
-        <CreateTodo></CreateTodo>
+        <Routes>
+          <Route
+            element={<ContentContainer></ContentContainer>}
+            path="/"
+          ></Route>
+          <Route
+            element={<CreateTodo></CreateTodo>}
+            path="/create-todo"
+          ></Route>
+        </Routes>
       </div>
     </>
-  );  
+  );
 }
 
 export default App;
